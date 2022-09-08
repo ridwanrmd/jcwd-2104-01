f924ef5c6b5627ded5c7b1d74d76e162a20ef7b7
ridwanrmd <ridwan95rmd@gmail.com>
2022-09-08 10:15:27 +0700

    merge latest update at main repo to local success

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -3,15 +3,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 const cors = require('cors');
-const userRouter = require('./routers/user');
 
-app.use('/api/public', express.static('public'));
 // Routers
 const userRouter = require('./routers/user');
-const { error } = require('console');
 
 // Config
 app.use(cors());
+app.use('/public', express.static('public'));
 app.use(express.json());
 
 app.get('/api', (req, res) => {
@@ -21,7 +19,6 @@ app.get('/api', (req, res) => {
 app.use('/users', userRouter);
 
 app.use((error, req, res, next) => {
-
   const errorObj = {
     status: 'Error',
     message: error.message,
