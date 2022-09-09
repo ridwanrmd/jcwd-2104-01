984b072ba4669ae8af0afddd14f1a397eb45a67e
Purwadhika Fullstack <93514408+purwadhikafullstack@users.noreply.github.com>
2022-09-09 10:25:17 +0700

    Update deploy.config.js

--- deploy.config.js
+++ deploy.config.js
@@ -2,6 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'JCWD-2104-01', // Format JCWD-{batchcode}-{groupnumber}
+      script: 'npm',
+      args: 'start',
       time: true,
     },
   ],
