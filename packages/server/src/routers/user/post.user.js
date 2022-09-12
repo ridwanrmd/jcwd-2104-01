8a6f22c518cc6438ce26e1bff14a2387f66555ee
ridwanrmd <ridwan95rmd@gmail.com>
2022-09-13 00:25:58 +0700

    merge conflict management

--- packages/server/src/routers/user/post.user.js
+++ packages/server/src/routers/user/post.user.js
@@ -2,13 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { isFieldEmpties, passwordValidator } = require('../../helpers');
 const validator = require('email-validator');
-<<<<<<< HEAD
-const { hash } = require('../../lib/bcrypt');
-const { createToken } = require('../../lib/token');
-const { sendMail } = require('../../lib/nodemailer');
-const { user } = require('../../../models');
-const { compare } = require('../../lib/bycrypt');
-=======
 const { compare, hash } = require('../../lib/bcrypt');
 const { createToken } = require('../../lib/token');
 const { sendMail } = require('../../lib/nodemailer');
@@ -18,122 +11,6 @@ const uploadUser = require('../../lib/multer');
 const fs = require('fs');
 const path = require('path');
 const appRoot = require('app-root-path');
-// const { compare } = require('../../lib/bycrypt');
-
-// register endpoint
-const registerUserHandler = async (req, res, next) => {
-  try {
-    const { first_name, last_name, email, phone, password, confirmPassword } =
-      req.body;
-    const emptyFields = isFieldEmpties({
-      first_name,
-      last_name,
-      email,
-      phone,
-      password,
-      confirmPassword,
-    });
-    if (emptyFields.length) {
-      throw {
-        code: 400,
-        message: `Empty fields: ${emptyFields}`,
-        data: { result: emptyFields },
-      };
-    }
-    // checking email
-    const emailValidator = validator.validate(email);
-    if (!emailValidator) {
-      throw {
-        code: 400,
-        message: 'Wrong email format',
-      };
-    }
-
-    //checking password
-    const validatePassword = passwordValidator(password);
-    if (validatePassword)
-      throw {
-        code: 400,
-        message: validatePassword,
-      };
-
-    // match password
-    if (password !== confirmPassword)
-      throw { code: 400, message: "Password didn't match" };
-
-    // check exist email
-    const resGetUser = await user.findOne({
-      where: { email },
-    });
-    if (resGetUser) {
-      const user = resGetUser.dataValues;
-      if (user.email == email) {
-        throw {
-          code: 400,
-          message: 'Email is already use',
-        };
-      }
-    }
-
-    // hash password
-    const encryptedPassword = hash(password);
-
-    //post user
-    const resCreateUser = await user.create({
-      first_name,
-      last_name,
-      email,
-      phone,
-      // image: "/public/avatar/default-avatar.png",
-      password: encryptedPassword,
-    });
-
-    const userId = resCreateUser.dataValues.userId;
-
-    //cretae token
-    const token = createToken({ userId });
-    const saveToken = await user.update({ token }, { where: { userId } });
-
-    //send email
-    await sendMail({ email, token });
-
-    res.send({
-      status: 'Success',
-      message: 'Succes create new user',
-      data: {
-        result: resCreateUser,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-// resend email verification endpoint
-const resendEmailVerification = async (req, res, next) => {
-  try {
-    const { email, userId } = req.body;
-
-    // create new token
-    const token = createToken({ userId });
-
-    // update new token to db
-    const updateToken = await user.update({ token }, { where: { userId } });
-
-    // sending new email verification
-    await sendMail({ email, token });
-
-    res.send({
-      status: 'Success',
-      message: 'Succes send new email verification',
-      data: {
-        result: updateToken,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-};
->>>>>>> 252bb1485078b2c5135f5add62e9872047dbc01b
 
 // register endpoint
 const registerUserHandler = async (req, res, next) => {
@@ -214,7 +91,7 @@ const registerUserHandler = async (req, res, next) => {
 
     res.send({
       status: 'Berhasil',
-      message: 'Berhasil membuat akun baru',
+      message: 'Periksa email anda untuk verifikasi akun',
       data: {
         result: resCreateUser,
       },
@@ -305,8 +182,6 @@ const loginUserController = async (req, res, next) => {
   }
 };
 
-<<<<<<< HEAD
-=======
 router.post('/upload', auth, uploadUser.single('gambar'), async (req, res) => {
   try {
     const { userId } = req.user;
@@ -334,7 +209,6 @@ router.post('/upload', auth, uploadUser.single('gambar'), async (req, res) => {
   }
 });
 
->>>>>>> 252bb1485078b2c5135f5add62e9872047dbc01b
 router.post('/register', registerUserHandler);
 router.post('/verification', resendEmailVerification);
 router.post('/login', loginUserController);
