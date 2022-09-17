0cbaa98f396ad08aa2dcff6d6339d62d3aa4042c
ridwanrmd <ridwan95rmd@gmail.com>
2022-09-17 23:52:25 +0700

    change rajaongkir key

--- packages/server/src/routers/rajaongkir/get.rajaongkir.js
+++ packages/server/src/routers/rajaongkir/get.rajaongkir.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 //config axios to rajaongkir
 axios.defaults.baseURL = 'https://api.rajaongkir.com/starter';
-axios.defaults.headers.common['key'] = '205258f7c4c0e26bd2a06dbbcabd2ca5';
+axios.defaults.headers.common['key'] = '4b336e545b4ba721481832b813a972b0';
 axios.defaults.headers.post['Content-Type'] =
   'application/x-www-form-urlencoded';
 
