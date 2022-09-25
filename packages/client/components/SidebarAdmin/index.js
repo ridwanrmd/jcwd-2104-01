9448b7576df5b0c2d5154cc843bb2019b99c2d7b
ridwanrmd <ridwan95rmd@gmail.com>
2022-09-25 21:42:46 +0700

    minor update

---
+++ packages/client/components/SidebarAdmin/index.js
@@ -0,0 +1,12 @@
+import { Box, Text } from '@chakra-ui/react';
+import React from 'react';
+
+function SidebarAdmin() {
+  return (
+    <Box>
+      <Text>test</Text>
+    </Box>
+  );
+}
+
+export default SidebarAdmin;
